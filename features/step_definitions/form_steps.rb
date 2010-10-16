43c667d963403d7eae273ca58856b4a6d4aa4f6d
Mike Burns <mburns@thoughtbot.com>
2010-10-16 10:37:22 -0400

    Some form steps

--- features/step_definitions/form_steps.rb
+++ features/step_definitions/form_steps.rb
@@ -1,43 +1,47 @@
 When 'I fill in "$phone_number" as my phone number' do |phone_number|
-  pending # express the regexp above with the code you wish you had
+  fill_in 'user[phone_number]', :with => phone_number
 end
 
 When 'I press the sign up button' do
-  pending # express the regexp above with the code you wish you had
+  click_button 'Sign up'
 end
 
 When 'I fill in my secret code' do
-  pending # express the regexp above with the code you wish you had
+  fill_in 'user[secret_code]', :with => secret_code
 end
 
 When 'I fill in the date of birth with "$date_of_birth"' do |date_of_birth|
-  pending # express the regexp above with the code you wish you had
+  fill_in 'user[dob]', :with => date_of_birth
 end
 
 When 'I fill in my name as "$name"' do |name|
-  pending # express the regexp above with the code you wish you had
+  fill_in 'user[name]', :with => name
 end
 
 When 'I fill in my description as "$description"' do |description|
-  pending # express the regexp above with the code you wish you had
+  fill_in 'user[description]', :with => description
 end
 
 When 'I check my gender as male' do
-  pending # express the regexp above with the code you wish you had
+  check 'user[male]'
 end
 
 When 'I fill in the minimum age with "$min_age"' do |min_age|
-  pending # express the regexp above with the code you wish you had
+  fill_in 'user[looking_for_minimum_age]', :with => min_age
 end
 
 When 'I fill in the maximum age with "$max_age"' do |max_age|
-  pending # express the regexp above with the code you wish you had
+  fill_in 'user[looking_for_maximum_age]', :with => max_age
 end
 
 When 'I check my desired gender as female' do
-  pending # express the regexp above with the code you wish you had
+  check 'user[looking_for_female]'
 end
 
 When 'I submit my profile' do
-  pending # express the regexp above with the code you wish you had
+  click_button 'Get in there'
+end
+
+def secret_code
+  nil # needs to pull it from a text message
 end
