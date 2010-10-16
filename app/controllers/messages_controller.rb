e75300480694c0a5e774992a9a32f28f81074be6
Mike Burns <mburns@thoughtbot.com>
2010-10-16 09:08:09 -0400

    More guesswork: tell the user and the date

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,10 @@ class MessagesController < ApplicationController
   before_filter :must_be_sms
 
   def index
-    if @user = User.find_by_phone_number(phone_number)
-      @date = @user.schedule_date_in(params[:session][:initialText])
+    if user = User.find_by_phone_number(phone_number)
+      @date = user.schedule_date_in(params[:session][:initialText])
       if @date.save
-        render :json => date_response_message
+        render :json => date_response_message_for(user)
       else
         render :json => failed_to_save_date_message
       end
@@ -21,10 +21,14 @@ class MessagesController < ApplicationController
     render :json => must_be_sms_message if network != 'SMS'
   end
 
-  def date_response_message
+  def date_response_message_for(user)
     if @date.scheduled?
-      ### Also tell the date
+      # Tell the user
+      @user = user
       send_sms(render :action => 'date_complete')
+      # Tell the date
+      @user = @date.for(user)
+      send_sms(render(:action => 'date_complete'), :to => @user.phone_number)
     else
       ### Also enqueue a follow-up 15m later
       send_sms(render :action => 'date_pending')
@@ -47,7 +51,13 @@ class MessagesController < ApplicationController
     params[:session].try(:[],:from).try(:[], :id)
   end
 
-  def send_sms(s)
-    Tropo::Generator.say(s)
+  def send_sms(s, opts = {})
+    if opts[:to]
+      Tropo::Generator.new do
+        message :message => s, :to => "tel:+1#{opts[:to]}"
+      end
+    else
+      Tropo::Generator.say(s)
+    end
   end
 end
