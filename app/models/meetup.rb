b4af96b71a99b17972e8b6a595f82e59d0556435
Mike Burns <mburns@thoughtbot.com>
2010-10-16 09:55:14 -0400

    Fill in some matchmaking methods with some code that might as well work

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,12 +1,61 @@
 class Meetup < ActiveRecord::Base
+  belongs_to :first_user, :class => 'User'
+  belongs_to :second_user, :class => 'User'
+
+  validates_presence_of :first_user_id
+
+  # All the unscheduled dates.
+  def self.unscheduled
+    where('second_user_id IS NOT NULL AND location IS NOT NULL')
+  end
+
+  # All dates near a place.
+  ### TODO: Geocoding stuff.
+  def self.near(place)
+    where('location = ?', place)
+  end
+
+  def self.within_age_range(min, max)
+    joins('JOIN users ON users.id = first_user_id').
+      where('NOW() - users.dob >= ? AND NOW() - users.dob <= max', min, max)
+  end
+
+  def self.looking_for(user)
+    joins('JOIN users ON users.id = first_user_id').
+      where('(users.looking_for_male = ? OR users.looking_for_female = ? OR users.looking_for_other = ?) AND users.looking_for_minimum_age >= ? AND users.looking_for_maximum_age <= ?',
+            user.male, user.female, user.other, user.age_in_years, user.age_in_years)
+  end
+
+  def self.men
+    joins('JOIN users ON users.id = first_user_id').where('users.male')
+  end
+
+  def self.women
+    joins('JOIN users ON users.id = first_user_id').where('users.female')
+  end
+
+  def self.other
+    joins('JOIN users ON users.id = first_user_id').where('users.other')
+  end
+
   # In a scheduled date produce the user that is opposite the one passed in.
   # In an unscheduled date produce nil.
   def for(user)
-    nil
+    if scheduled?
+      if first_user_id == user.id
+        second_user
+      elsif second_user_id == user.id
+        first_user
+      end
+    end
   end
 
   # True if the date has two people and a meeting spot.
   def scheduled?
-    false
+    second_user_id.present? && location.present?
+  end
+
+  def unscheduled?
+    ! scheduled?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ActiveRecord::Base
   attr_protected :secret_code, :phone_number
 
+  has_many :founded_meetups, :class => 'Meetup', :foreign_key => 'first_user_id'
+
   # The magic of finding a match and making a date.
   # Produces either a scheduled or an unscheduled meetup.
   def schedule_date_in(location)
-    Meetup.new
+    meetup_finder_near(location).first || founded_meetups.build(:location => location)
+  end
+
+  protected
+
+  def meetup_finder_near(location)
+    finder = Meetup.unscheduled.
+      near(location).
+      within_age_range(self.looking_for_minimum_age, self.looking_for_maximum_age).
+      looking_for(self)
+    finder = finder.men   if self.looking_for_male
+    finder = finder.women if self.looking_for_female
+    finder = finder.other if self.looking_for_other
+    finder
   end
 end
