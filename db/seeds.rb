7a4d13c5e74b2d1a47b9b2b9dfd9d103e03bf733
Chad Mazzola <cmazzola@thoughtbot.com>
2010-10-16 19:57:37 -0400

    Add more locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,14 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 DateSuggestion.create([
-  { :text => "Silvertone" },
-  { :text => "Mike's Apartment" },
-  { :text => "Walmart" }
+  { :text => "Silvertone (69 Bromfield St, Boston. Near Park St Station)" },
+  { :text => "Russell House Tavern (14 JFK St, Cambridge. Near Harvard Station)" },
+  { :text => "Green Street Grill (280 Green St, Cambridge. Near Central Station)" },
+  { :text => "American Craft (1700 Beacon St, Brookline. Near Washington Square)" },
+  { :text => "Stoddard's (48 Temple Pl, Boston. Near Park St Station)" },
+  { :text => "Tory Row (3 Brattle Street, Cambridge. Near Harvard Station)" },
+  { :text => "1369 Coffeehouse (757 Mass Ave, Cambridge. Near Central Station)" },
+  { :text => "Diesel Cafe (257 Elm St, Somerville. Near Davis Station)" },
+  { :text => "Harvard Book Store (1256 Mass Ave, Cambridge. Near Harvard Station)" },
+  { :text => "JP Licks (352 Newbury St, Boston. Near Hynes Station)" }
 ])
