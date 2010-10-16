c3a561fe105e7c21ffc4f067d836355934e61f01
Mike Burns <mburns@thoughtbot.com>
2010-10-16 10:24:44 -0400

    Fill in some step definitions with empty stuff

---
+++ features/step_definitions/form_steps.rb
@@ -0,0 +1,43 @@
+When 'I fill in "$phone_number" as my phone number' do |phone_number|
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I press the sign up button' do
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I fill in my secret code' do
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I fill in the date of birth with "$date_of_birth"' do |date_of_birth|
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I fill in my name as "$name"' do |name|
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I fill in my description as "$description"' do |description|
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I check my gender as male' do
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I fill in the minimum age with "$min_age"' do |min_age|
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I fill in the maximum age with "$max_age"' do |max_age|
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I check my desired gender as female' do
+  pending # express the regexp above with the code you wish you had
+end
+
+When 'I submit my profile' do
+  pending # express the regexp above with the code you wish you had
+end
