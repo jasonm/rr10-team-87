90743322d5c8c0adb316d1c3a888995e00216d1d
Emma Lindsay <elindsay@thoughtbot.com>
2010-10-15 22:39:03 -0400

    Initial spike into messaging

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'tropo-webapi-ruby'
 
 # Initialize the rails application
 Instalover::Application.initialize!
