23a42eb361ab77936fbb3433a9148f3d26da59bc
Jason Morrison <jmorrison@thoughtbot.com>
2010-10-15 20:21:49 -0400

    suspend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instalover::Application.config.secret_token = '6522ec82a191e28ee478d9077db799a438ec19d5f33a65ce752794067026363a4efa053099e95c05ed7d464925581c4fab2865cb74acc73eb83e30288cbffb15'
