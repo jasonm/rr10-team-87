cccd2b9156718630fe16de2e94b7bb068ba3cf13
Mike Burns <mburns@thoughtbot.com>
2010-10-17 10:41:27 -0400

    Jobs must be placed into a queue

--- app/models/message_sender.rb
+++ app/models/message_sender.rb
@@ -1,6 +1,8 @@
 class MessageSender < Resque::Plugins::RestrictionJob
   restrict :per_minute => 10
 
+  @queue = :messages
+
   def self.perform(uri_string)
     Net::HTTP.get(URI.parse(uri_string))
   end
