1f3d60e19af64dfc821fd98b6af88c803eaa7cf3
Chad Mazzola <cmazzola@thoughtbot.com>
2010-10-17 12:35:46 -0400

    Initial push of twitter on home page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
   if ($('form#new_user li.error').size() > 0) {
     $('form#new_user').show();
   }
+
+  $("#tweets").tweet({
+    username: "instalover",
+    join_text: "auto",
+    avatar_size: 0,
+    count: 1,
+    query: "#dfln",
+    auto_join_text_default: "",
+    auto_join_text_ed: "we",
+    auto_join_text_ing: "we were",
+    auto_join_text_reply: "we replied to",
+    auto_join_text_url: "we were checking out",
+    loading_text: "loading tweets..."
+  });
 });
