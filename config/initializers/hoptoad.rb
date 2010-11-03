548750e28e7c1e9eb4b3ec8ce2f39dab10f569fb
Jason Morrison <jmorrison@thoughtbot.com>
2010-11-03 18:27:49 -0400

    Use Resque::Failure::Hoptoad for failed resque jobs

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,7 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = 'f12b4bfa6c4e2ea65f55e8f871ccf1f3'
+require 'resque/failure/hoptoad'
+
+[HoptoadNotifier, Resque::Failure::Hoptoad].each do |notifier|
+  notifier.configure do |config|
+    config.api_key = 'f12b4bfa6c4e2ea65f55e8f871ccf1f3'
+  end
 end
